385334c873dbecd5ccfe1aa02edd36bf57cac383
EricWarnemundeVertina <73593894+EricWarnemundeVertina@users.noreply.github.com>
2025-07-18 13:43:24 -0400

    Update Generate_MXenes.py

--- Generate_MXenes.py
+++ Generate_MXenes.py
@@ -116,7 +116,7 @@ def save_generated_MXene(save_path, save_filename, lattice_a, lattice_c, atom_li
     return 'Done!'
 
 
-def generate_MXene(configuration_type, num_MX_atoms, dissertation_avg_M_X_bonds_df, dissertation_avg_M_Tx_bonds_df, dissertation_avg_Tx_Tx_bonds_df, x_frac_list, y_frac_list, Tx_x_frac_list, Tx_y_frac_list, theta_MX_list, theta_MTx_list, phi_MX_list, phi_MTx_list, atom_list, desired_layer_distance, num_Tx, Tx_atoms_list):
+def generate_MXene(configuration_type, num_MX_atoms, avg_M_X_bonds_df, avg_M_Tx_bonds_df, avg_Tx_Tx_bonds_df, x_frac_list, y_frac_list, Tx_x_frac_list, Tx_y_frac_list, theta_MX_list, theta_MTx_list, phi_MX_list, phi_MTx_list, atom_list, desired_layer_distance, num_Tx, Tx_atoms_list):
 
     min_bond_len = 4
     longest_bond_pair_indexes = []
@@ -124,26 +124,26 @@ def generate_MXene(configuration_type, num_MX_atoms, dissertation_avg_M_X_bonds_
     for i in range(len(atom_list) - 1):
         if num_Tx == 2 and (i == 0 or i == len(atom_list) - 2):  # do not want to base the cell on the Tx-Tx bond length
             if Tx_atoms_list[0] == 'H' and Tx_atoms_list[1] in ['F','Cl','Br','I']:
-                new_bond = dissertation_avg_Tx_Tx_bonds_df.loc[Tx_atoms_list[0], Tx_atoms_list[1]]
+                new_bond = avg_Tx_Tx_bonds_df.loc[Tx_atoms_list[0], Tx_atoms_list[1]]
             else:
-                new_bond = dissertation_avg_Tx_Tx_bonds_df.loc[Tx_atoms_list[1], Tx_atoms_list[0]]
+                new_bond = avg_Tx_Tx_bonds_df.loc[Tx_atoms_list[1], Tx_atoms_list[0]]
         elif (num_Tx == 2 and i == 1) or (num_Tx == 1 and i == 0):  # bottom M-Tx bond
-            new_bond = dissertation_avg_M_Tx_bonds_df.loc[atom_list[i + 1], atom_list[i]]
+            new_bond = avg_M_Tx_bonds_df.loc[atom_list[i + 1], atom_list[i]]
             if not ('t' in configuration_type) and new_bond < min_bond_len:
                 min_bond_len = new_bond
                 longest_bond_pair_indexes = i
         elif (num_Tx == 2 and i == len(atom_list) - 3) or (num_Tx == 1 and i == len(atom_list) - 2):  # top M-Tx bond
-            new_bond = dissertation_avg_M_Tx_bonds_df.loc[atom_list[i], atom_list[i + 1]]
+            new_bond = avg_M_Tx_bonds_df.loc[atom_list[i], atom_list[i + 1]]
             if not ('t' in configuration_type) and new_bond < min_bond_len:
                 min_bond_len = new_bond
                 longest_bond_pair_indexes = i
         elif (i-num_Tx)%2 == 0: # M element
-            new_bond = dissertation_avg_M_X_bonds_df.loc[atom_list[i], atom_list[i + 1]]
+            new_bond = avg_M_X_bonds_df.loc[atom_list[i], atom_list[i + 1]]
             if new_bond < min_bond_len:
                 min_bond_len = new_bond
                 longest_bond_pair_indexes = i
         else: # X element
-            new_bond = dissertation_avg_M_X_bonds_df.loc[atom_list[i + 1], atom_list[i]]
+            new_bond = avg_M_X_bonds_df.loc[atom_list[i + 1], atom_list[i]]
             if new_bond < min_bond_len:
                 min_bond_len = new_bond
                 longest_bond_pair_indexes = i
@@ -200,29 +200,29 @@ def generate_MXene(configuration_type, num_MX_atoms, dissertation_avg_M_X_bonds_
                 z_list.append(zprev + desired_layer_distance/2)
                 if num_Tx == 2:
                     if atom_list[i] == 'H' and atom_list[i + 1] in ['F','Cl','Br','I']:
-                        new_bond = dissertation_avg_Tx_Tx_bonds_df.loc[atom_list[i], atom_list[i + 1]]
+                        new_bond = avg_Tx_Tx_bonds_df.loc[atom_list[i], atom_list[i + 1]]
                     else:
-                        new_bond = dissertation_avg_Tx_Tx_bonds_df.loc[atom_list[i + 1], atom_list[i]]
+                        new_bond = avg_Tx_Tx_bonds_df.loc[atom_list[i + 1], atom_list[i]]
                     xnext_frac, ynext_frac = xprev_frac, yprev_frac
                     xnext, ynext = xprev, yprev  # outermost Tx atom in Tx-Tx bond has same x and y coordinates as other Tx atom
                 else:  # num_Tx is 1    # M-Tx bond
-                    new_bond = dissertation_avg_M_Tx_bonds_df.loc[atom_list[i + 1], atom_list[i]]
+                    new_bond = avg_M_Tx_bonds_df.loc[atom_list[i + 1], atom_list[i]]
                     xnext_frac, ynext_frac = x_frac_list[0], y_frac_list[0]
                     xnext, ynext = get_x(lattice_a, xnext_frac, ynext_frac), get_y(lattice_a, ynext_frac)
             elif i == 1:  # 2nd Tx-Tx atom
-                new_bond = dissertation_avg_M_Tx_bonds_df.loc[atom_list[i + 1], atom_list[i]]
+                new_bond = avg_M_Tx_bonds_df.loc[atom_list[i + 1], atom_list[i]]
                 xnext_frac, ynext_frac = x_frac_list[0], y_frac_list[0]
                 xnext, ynext = get_x(lattice_a, xnext_frac, ynext_frac), get_y(lattice_a, ynext_frac)
         elif num_Tx == 2 and i == len(atom_list) - num_Tx:   # bottommost Tx atoms in top Tx-Tx bond
             if atom_list[i + 1] == 'H' and atom_list[i] in ['F','Cl','Br','I']:
-                new_bond = dissertation_avg_Tx_Tx_bonds_df.loc[atom_list[i + 1], atom_list[i]]
+                new_bond = avg_Tx_Tx_bonds_df.loc[atom_list[i + 1], atom_list[i]]
             else:
-                new_bond = dissertation_avg_Tx_Tx_bonds_df.loc[atom_list[i], atom_list[i + 1]]
+                new_bond = avg_Tx_Tx_bonds_df.loc[atom_list[i], atom_list[i + 1]]
             xnext_frac, ynext_frac = xprev_frac, yprev_frac
             xnext, ynext = xprev, yprev
         else:  # M atom or X atom
             if i == 0:  # bare MXene
-                new_bond = dissertation_avg_M_X_bonds_df.loc[atom_list[i], atom_list[i + 1]]
+                new_bond = avg_M_X_bonds_df.loc[atom_list[i], atom_list[i + 1]]
                 xprev_frac, yprev_frac = x_frac_list[0], y_frac_list[0]
                 xprev, yprev = get_x(lattice_a, xprev_frac, yprev_frac), get_y(lattice_a, yprev_frac)
                 xnext_frac, ynext_frac = x_frac_list[1], y_frac_list[1]
@@ -232,18 +232,18 @@ def generate_MXene(configuration_type, num_MX_atoms, dissertation_avg_M_X_bonds_
                 y_frac_list_generated.append(yprev_frac)
                 z_list.append(zprev + desired_layer_distance/2)
             elif i == num_MX_atoms + num_Tx - 1:  # top M-Tx bond
-                new_bond = dissertation_avg_M_Tx_bonds_df.loc[atom_list[i], atom_list[i + 1]]
+                new_bond = avg_M_Tx_bonds_df.loc[atom_list[i], atom_list[i + 1]]
                 xnext_frac, ynext_frac = Tx_x_frac_list[1], Tx_y_frac_list[1]
                 xnext, ynext = get_x(lattice_a, xnext_frac, ynext_frac), get_y(lattice_a, ynext_frac)
             elif (i-num_Tx)%2 == 0:  # M atom
-                new_bond = dissertation_avg_M_X_bonds_df.loc[atom_list[i], atom_list[i + 1]]
+                new_bond = avg_M_X_bonds_df.loc[atom_list[i], atom_list[i + 1]]
                 if 'ABA' in configuration_type:  # ABA
                     xnext_frac, ynext_frac = x_frac_list[1], y_frac_list[1]
                 else:  # ABC
                     xnext_frac, ynext_frac = x_frac_list[(i-num_Tx+1)%3], y_frac_list[(i-num_Tx+1)%3]
                 xnext, ynext = get_x(lattice_a, xnext_frac, ynext_frac), get_y(lattice_a, ynext_frac)
             else: # X atom
-                new_bond = dissertation_avg_M_X_bonds_df.loc[atom_list[i + 1], atom_list[i]]
+                new_bond = avg_M_X_bonds_df.loc[atom_list[i + 1], atom_list[i]]
                 if 'ABA' in configuration_type:  # ABA
                     xnext_frac, ynext_frac = x_frac_list[0], y_frac_list[0]
                 else:  # ABC
@@ -316,19 +316,19 @@ def get_avg_bond_dfs(directory, avg_M_X_bonds_name, avg_M_Tx_bonds_name, avg_Tx_
         avg_M_Tx_bonds_filename = avg_M_Tx_bonds_name + '_' + n_value + '.csv'
         avg_Tx_Tx_bonds_filename = avg_Tx_Tx_bonds_name + '_' + n_value + '.csv'
 
-    dissertation_avg_M_X_bonds_df = pd.read_csv(directory + avg_M_X_bonds_filename)
-    column_names = dissertation_avg_M_X_bonds_df.columns.tolist()
-    dissertation_avg_M_X_bonds_df = dissertation_avg_M_X_bonds_df.set_index(column_names[0])
+    avg_M_X_bonds_df = pd.read_csv(directory + avg_M_X_bonds_filename)
+    column_names = avg_M_X_bonds_df.columns.tolist()
+    avg_M_X_bonds_df = avg_M_X_bonds_df.set_index(column_names[0])
 
-    dissertation_avg_M_Tx_bonds_df = pd.read_csv(directory + avg_M_Tx_bonds_filename)
-    column_names = dissertation_avg_M_Tx_bonds_df.columns.tolist()
-    dissertation_avg_M_Tx_bonds_df = dissertation_avg_M_Tx_bonds_df.set_index(column_names[0])
+    avg_M_Tx_bonds_df = pd.read_csv(directory + avg_M_Tx_bonds_filename)
+    column_names = avg_M_Tx_bonds_df.columns.tolist()
+    avg_M_Tx_bonds_df = avg_M_Tx_bonds_df.set_index(column_names[0])
 
-    dissertation_avg_Tx_Tx_bonds_df = pd.read_csv(directory + avg_Tx_Tx_bonds_filename)
-    column_names = dissertation_avg_Tx_Tx_bonds_df.columns.tolist()
-    dissertation_avg_Tx_Tx_bonds_df = dissertation_avg_Tx_Tx_bonds_df.set_index(column_names[0])
+    avg_Tx_Tx_bonds_df = pd.read_csv(directory + avg_Tx_Tx_bonds_filename)
+    column_names = avg_Tx_Tx_bonds_df.columns.tolist()
+    avg_Tx_Tx_bonds_df = avg_Tx_Tx_bonds_df.set_index(column_names[0])
 
-    return dissertation_avg_M_X_bonds_df, dissertation_avg_M_Tx_bonds_df, dissertation_avg_Tx_Tx_bonds_df
+    return avg_M_X_bonds_df, avg_M_Tx_bonds_df, avg_Tx_Tx_bonds_df
 
 
 
@@ -426,15 +426,15 @@ save_path = args.save_path
 
 bond_directory = 'Average_bond_lengths/'  # directory to avg bond .csv's
 
-avg_M_X_bonds_name = 'dissertation_avg_M_X_bonds'
-avg_M_Tx_bonds_name = 'dissertation_avg_M_Tx_bonds'
-avg_Tx_Tx_bonds_name = 'dissertation_avg_Tx_Tx_bonds'
+avg_M_X_bonds_name = 'avg_M_X_bonds'
+avg_M_Tx_bonds_name = 'avg_M_Tx_bonds'
+avg_Tx_Tx_bonds_name = 'avg_Tx_Tx_bonds'
 
 desired_layer_distance = 15
 
 
 
-dissertation_avg_M_X_bonds_df, dissertation_avg_M_Tx_bonds_df, dissertation_avg_Tx_Tx_bonds_df = get_avg_bond_dfs(bond_directory, avg_M_X_bonds_name, avg_M_Tx_bonds_name, avg_Tx_Tx_bonds_name, n_value)
+avg_M_X_bonds_df, avg_M_Tx_bonds_df, avg_Tx_Tx_bonds_df = get_avg_bond_dfs(bond_directory, avg_M_X_bonds_name, avg_M_Tx_bonds_name, avg_Tx_Tx_bonds_name, n_value)
 num_MX_atoms, num_X_atoms, x_frac_list, y_frac_list, Tx_x_frac_list, Tx_y_frac_list, theta_MX_list, theta_MTx_list, phi_MX_list, phi_MTx_list = get_fracs_phis_thetas(configuration_type, n_value)
 M_combinations_list, X_possibilities_list, Tx_possibilities_list = get_combos(M_dict, X_dict, Tx_dict, M_combination)
 
@@ -468,7 +468,7 @@ for selected_M_groups in M_combinations_list:
                 else:  # X atom
                     atom_list.append(X_atoms_list)
             
-            lattice_a, lattice_c, frac_coords = generate_MXene(configuration_type, num_MX_atoms, dissertation_avg_M_X_bonds_df, dissertation_avg_M_Tx_bonds_df, dissertation_avg_Tx_Tx_bonds_df, x_frac_list, y_frac_list, Tx_x_frac_list, Tx_y_frac_list, theta_MX_list, theta_MTx_list, phi_MX_list, phi_MTx_list, atom_list, desired_layer_distance, num_Tx, Tx_atoms_list)
+            lattice_a, lattice_c, frac_coords = generate_MXene(configuration_type, num_MX_atoms, avg_M_X_bonds_df, avg_M_Tx_bonds_df, avg_Tx_Tx_bonds_df, x_frac_list, y_frac_list, Tx_x_frac_list, Tx_y_frac_list, theta_MX_list, theta_MTx_list, phi_MX_list, phi_MTx_list, atom_list, desired_layer_distance, num_Tx, Tx_atoms_list)
 
             save_generated_MXene(save_path + '/', save_filename, lattice_a, lattice_c, atom_list, frac_coords)
             #save_generated_MXene(fake_save_path, save_filename, lattice_a, lattice_c, atom_list, frac_coords)
@@ -499,7 +499,7 @@ for selected_M_groups in M_combinations_list:
                     else:  # X atom
                         atom_list.append(X_atoms_list)
                 
-                lattice_a, lattice_c, frac_coords = generate_MXene(configuration_type, num_MX_atoms, dissertation_avg_M_X_bonds_df, dissertation_avg_M_Tx_bonds_df, dissertation_avg_Tx_Tx_bonds_df, x_frac_list, y_frac_list, Tx_x_frac_list, Tx_y_frac_list, theta_MX_list, theta_MTx_list, phi_MX_list, phi_MTx_list, atom_list, desired_layer_distance, num_Tx, Tx_atoms_list)
+                lattice_a, lattice_c, frac_coords = generate_MXene(configuration_type, num_MX_atoms, avg_M_X_bonds_df, avg_M_Tx_bonds_df, avg_Tx_Tx_bonds_df, x_frac_list, y_frac_list, Tx_x_frac_list, Tx_y_frac_list, theta_MX_list, theta_MTx_list, phi_MX_list, phi_MTx_list, atom_list, desired_layer_distance, num_Tx, Tx_atoms_list)
 
                 save_generated_MXene(save_path + '/', save_filename, lattice_a, lattice_c, atom_list, frac_coords)
                 #save_generated_MXene(fake_save_path, save_filename, lattice_a, lattice_c, atom_list, frac_coords)
